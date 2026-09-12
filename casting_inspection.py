139e5097c3f0897145d18482df3baf9977b665eb
rogerbes-dev <rogerbes@gmail.com>
2026-09-12 10:09:36 -0300

    Readme e addição de perguntas

--- casting_inspection.py
+++ casting_inspection.py
@@ -29,6 +29,11 @@ BATCH_SIZE = 32
 EPOCHS = 15
 DATASET_DIR = "dataset_casting"  # Diretório contendo as pastas 'ok' e 'def_front'
 
+"""
+### PERGUNTA 1: 
+# Qual o objetivo do sistema e demonstração de funcionamento no notebook?
+"""
+
 def run_opencv_exploratory_analysis(sample_image_path):
 
 #============================================================================
@@ -92,6 +97,12 @@ def run_opencv_exploratory_analysis(sample_image_path):
     plt.savefig("pipeline_opencv_exploratorio.png", dpi=300)
     plt.close()
     print("[SUCESSO] Painel da análise exploratória OpenCV salvo em 'pipeline_opencv_exploratorio.png'.")
+
+
+"""
+### PERGUNTA 2:
+# O que as técnicas clássicas de OpenCV (Canny, Blur) revelaram sobre os defeitos da peça na sua análise exploratória?  
+"""
 
 def build_and_train_cnn(dataset_dir):
 
@@ -176,6 +187,11 @@ def build_and_train_cnn(dataset_dir):
         epochs=EPOCHS
     )    
 
+"""
+### PERGUNTA 3:
+# 3. Como você estruturou a sua CNN e o Data Augmentation para aprender esses padrões em larga escala?
+"""
+
     # SPRINT 6: Geração de gráficos de loss e acurácia (Curvas de Treinamento)
     print("[INFO] Executando Sprint 6: Gerando Gráficos de Auditoria Gráfica (Loss e Accuracy)...")
     acc = history.history['accuracy']
@@ -225,3 +241,8 @@ if __name__ == "__main__":
     
     # 2. Executa a Ingestão e o Treinamento da CNN (TensorFlow)
     build_and_train_cnn('dataset_casting')
+
+"""
+### PERGUNTA 4:
+# Analisando o seu gráfico de Loss gerado no final, ocorreu Overfitting ou o modelo aprendeu de forma saudável?
+"""
